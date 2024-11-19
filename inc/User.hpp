1fb1847497dd768e04797dfbdec52f455ed3cc1d
Maksim Kramer <mkramer@c1r1p5.42quebec.com>
2024-11-18 19:37:42 -0500

    add message and user

---
+++ inc/User.hpp
@@ -0,0 +1,10 @@
+#include "irc.hpp"
+class User {
+    public:
+        User();
+        ~User();
+        User(User const & src);
+        User & operator=(User const & src);
+    private:
+        bool isOperator;
+};
--- src/Request.cpp
+++
@@ -1,3 +0,0 @@
-#include "../inc/Request.hpp"
-Request::Request(): _method("NONE") {
-}
