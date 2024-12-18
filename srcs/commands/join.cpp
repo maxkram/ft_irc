8fd51fc16fc93bb2f61500edec808db97e9c5e82
Maksim Kramer <mkramer@c1r1p5.42quebec.com>
2024-12-17 22:29:28 -0500

    update join

--- srcs/commands/join.cpp
+++ srcs/commands/join.cpp
@@ -2,16 +2,19 @@
 
 void	Command::join(std::vector<std::string> cmds, Client & client)
 {
+	// Step 1: Client registration check
 	if (!client.isRegistered())
 	{
 		sendMessage(client, "451", "", ERR_NOTREGISTERED);
 		return ;
 	}
+	// Step 2: Check for sufficient parameters
 	if (cmds.size() < 2)
 	{
 		sendMessage(client, "461", cmds[0], ERR_NEEDMOREPARAMS);
 		return ;
 	}
+	// Step 3: Handle "JOIN 0" (leave all channels)
 	if (cmds[1] == "0")
 	{
 		std::vector<std::string>::iterator	it = client.getChannels().begin();
@@ -23,23 +26,27 @@ void	Command::join(std::vector<std::string> cmds, Client & client)
 		part(partCmd, client);
 		return;
 	}
-	std::vector<std::string>	chans = splitString(cmds[1], ",");
+	// Step 4: Split channels and keys
+	std::vector<std::string>	channels = splitString(cmds[1], ",");
 	std::vector<std::string>	keys;
 	std::vector<std::string>	names;
 	names.push_back("names");
 	names.push_back(cmds[1]);
 	if (cmds.size() > 2)
 		keys = splitString(cmds[2], ",");
-	std::vector<std::string>::iterator	it = chans.begin();
+	std::vector<std::string>::iterator	it = channels.begin();
 	std::vector<std::string>::iterator	itKey = keys.begin();
 	std::map<std::string, Channel>::iterator	itMap;
-	for(; it != chans.end(); it++)
+	for(; it != channels.end(); it++)
 	{
+		// Step 5: Channel name validation
 		if (it->length() > 50 || (*it)[0] != '#')
 			sendMessage(client, "403", cmds[0], *it + ERR_NOSUCHCHANNEL);
+		// Step 6: Create channel if it doesn't exist
 		else if ((itMap = chanMap.find(*it)) == chanMap.end())
 		{
 			itMap =	chanMap.insert(std::pair<std::string, Channel>(*it, Channel())).first;
+			// Set channel operator and optional password
 			itMap->second.addChannelOperator(&client);
 			if (itKey != keys.end())
 				itMap->second.setPassword(*itKey);
@@ -51,6 +58,7 @@ void	Command::join(std::vector<std::string> cmds, Client & client)
 		}
 		else
 		{
+			// Step 7: Verify access to existing channel
 			if (!client.isInChannel(*it))
 			{
 				if (!itMap->second.getPassword().empty() && (itKey == keys.end() || itMap->second.getPassword() != *itKey))
@@ -63,11 +71,13 @@ void	Command::join(std::vector<std::string> cmds, Client & client)
 				}
 				else
 				{
+					// Add client to the channel
 					itMap->second.addClient(&client);
 					client.getChannels().push_back(*it);
 					sendConfirm(client, cmds[0] + " " + *it, "");
 					sendMessage(client, "324", cmds[1], getUsedModes(cmds[1]));
 					Command::names(names, client);
+					// Send channel topic if it exists
 					if (!itMap->second.getTopic().empty())
 					{
 						sendMessage(client, "332", *it, itMap->second.getTopic());
