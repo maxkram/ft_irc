c70d50ec12d48d574e76edfd17daec39d09927b0
Maksim Kramer <mkramer@c1r1p5.42quebec.com>
2024-12-25 21:43:49 -0500

    update ping

--- srcs/commands/ping.cpp
+++ srcs/commands/ping.cpp
@@ -1,16 +1,20 @@
 #include "../../includes/server.hpp"
 
-void Server::PING(std::string &message, int fd)
-{
-    User *user;
-    std::vector<std::string> param;
+void Server::PING(std::string &message, int fd) {
+    User *user = getClientByFd(fd);
+    if (!user) {
+        return;
+    }
 
-    user = getClientByFd(fd);
-    param = extractParams(message);
-    if (user && param.size() < 2)
-    {
+    // Extract parameters from the message
+    std::vector<std::string> param = extractParams(message);
+
+    // Check if there are enough parameters
+    if (param.size() < 2) {
         notifyUsers(ERR_NOTENOUGHPARAMETERS(user->getNickname()), fd);
         return;
     }
+
+    // Respond with PONG
     notifyUsers(RPL_PONG, fd);
 }
