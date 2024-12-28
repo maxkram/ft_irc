8ef83c06731782c438c3a11f55edadf8c37a6695
Maksim Kramer <mkramer@c1r1p5.42quebec.com>
2024-12-27 22:09:00 -0500

    update user class

--- srcs/user.cpp
+++ srcs/user.cpp
@@ -1,21 +1,26 @@
 #include "../includes/user.hpp"
 
 User::User()
+    : fdUser(-1),
+      nickname(""),
+      user(""),
+      ip(""),
+      buff(""),
+      realname(""),
+      registered(false),
+      connected(false),
+      isOp(false),
+      chanFounder(false)
 {
-    this->fdUser = -1;
-    this->nickname = "";
-    this->user = "";
-    this->ip = "";
-    this->buff = "";
-    this->realname = "";
-    this->registered = false;
-    this->connected = false;
-    this->isOp = false;
-    this->chanFounder = false;
-    time(&this->created);
-    char modes[2] = {'i', 'o'};
-    for (int i = 0; i < 2; i++)
+    // Initialize creation time
+    time(&created);
+
+    // Define user modes
+    const char modes[] = {'i', 'o'};
+    for (size_t i = 0; i < sizeof(modes) / sizeof(modes[0]); ++i)
+    {
         userModes.push_back(std::make_pair(modes[i], false));
+    }
 }
 
 User::User(int fd, std::string nickname, std::string user)
@@ -28,74 +33,58 @@ User::User(User const &obj)
 
 User &User::operator=(User const &obj)
 {
-    if (this != &obj)
+    if (this != &obj) // Self-assignment guard
     {
-        this->fdUser = obj.fdUser;
-        this->nickname = obj.nickname;
-        this->user = obj.user;
-        this->ip = obj.ip;
-        this->buff = obj.buff;
-        this->registered = obj.registered;
-        this->connected = obj.connected;
-        this->isOp = obj.isOp;
-        this->userModes = obj.userModes;
-        this->realname = obj.realname;
-        this->chanFounder = obj.chanFounder;
-        this->created = obj.created;
+        // Copy primitive and standard types
+        fdUser = obj.fdUser;
+        nickname = obj.nickname;
+        user = obj.user;
+        ip = obj.ip;
+        buff = obj.buff;
+        registered = obj.registered;
+        connected = obj.connected;
+        isOp = obj.isOp;
+        chanFounder = obj.chanFounder;
+        created = obj.created;
+        realname = obj.realname;
+
+        // Copy container members
+        userModes = obj.userModes;
     }
     return *this;
 }
 
 bool User::operator==(User const &rhs)
 {
-    if (this->nickname == rhs.nickname)
-        return (true);
-    return (false);
+    return nickname == rhs.nickname;
 }
 
-User::~User()
-{}
+User::~User() {}
 
-int User::getFduser()
-{
-    return (this->fdUser);
-}
+int User::getFduser() { return (this->fdUser); }
 
-std::string &User::getNickname()
-{
-    return (this->nickname);
-}
+std::string &User::getNickname() { return (this->nickname); }
 
 std::string User::getUser()
 {
     return (user.empty() ? std::string("") : user);
 }
 
-std::string User::getIp()
-{
-    return (ip);
-}
+std::string User::getIp() { return (ip); }
 
-std::string User::getBuffer()
-{
-    return (buff);
-}
+std::string User::getBuffer() { return (buff); }
 
 std::string User::getHostname()
 {
-    std::string hostname = getNickname() + "!" + getUser();
-    return (hostname);
+    return getNickname() + "!" + getUser();
 }
 
-std::string User::getRealname() const
-{
-    return (this->realname);
-}
+std::string User::getRealname() const { return (this->realname); }
 
 std::string User::getUserModes()
 {
     std::string mode;
-    for (size_t i = 0; i < userModes.size(); i++)
+    for (size_t i = 0; i < userModes.size(); ++i)
     {
         if (userModes[i].first != 'o' && userModes[i].second)
             mode.push_back(userModes[i].first);
@@ -105,106 +94,52 @@ std::string User::getUserModes()
     return (mode);
 }
 
-bool User::isRegistered()
-{
-    return (registered);
-}
+bool User::isRegistered() { return (registered); }
 
-bool User::isConnected()
-{
-    return (this->connected);
-}
+bool User::isConnected() { return (this->connected); }
 
-bool User::isOperator()
-{
-    return (this->isOp);
-}
+bool User::isOperator() { return (this->isOp); }
 
 bool User::isInvited(std::string &channel)
 {
-    for (size_t i = 0; i < this->invitation.size(); i++)
+    for (size_t i = 0; i < invitation.size(); ++i)
     {
-        if (this->invitation[i] == channel)
+        if (invitation[i] == channel)
             return (true);
     }
     return (false);
 }
 
-bool User::getUserModeOption(size_t i)
-{
-    return (userModes[i].second);
-}
+bool User::getUserModeOption(size_t i) { return (userModes[i].second); }
 
-bool User::isChannelFounder()
-{
-    return (this->chanFounder);
-}
+bool User::isChannelFounder() { return (this->chanFounder); }
 
-time_t User::getCreationTime() const
-{
-    return (created);
-}
+time_t User::getCreationTime() const { return (created); }
 
-void User::setFduser(int fd)
-{
-    this->fdUser = fd;
-}
+void User::setFduser(int fd) { this->fdUser = fd; }
 
-void User::setNickname(std::string &nickname)
-{
-    this->nickname = nickname;
-}
+void User::setNickname(std::string &nickname) { this->nickname = nickname; }
 
-void User::setUser(std::string &user)
-{
-    this->user = user;
-}
+void User::setUser(std::string &user) { this->user = user; }
 
-void User::setIp(std::string ip)
-{
-    this->ip = ip;
-}
+void User::setIp(std::string ip) { this->ip = ip; }
 
-void User::setBuffer(std::string recv)
-{
-    buff += recv;
-}
+void User::setBuffer(std::string recv) { buff += recv; }
 
-void User::setRegistered(bool val)
-{
-    registered = val;
-}
+void User::setRegistered(bool val) { registered = val; }
 
-void User::setConnected(bool val)
-{
-    this->connected = val;
-}
+void User::setConnected(bool val) { this->connected = val; }
 
-void User::setOperator(bool op)
-{
-    this->isOp = op;
-}
+void User::setOperator(bool op) { this->isOp = op; }
 
-void User::setChannelFounder(bool founder)
-{
-    this->chanFounder = founder;
-}
+void User::setChannelFounder(bool founder) { this->chanFounder = founder; }
 
 
-void User::setUserMode(size_t i, bool mode)
-{
-    userModes[i].second = mode;
-}
+void User::setUserMode(size_t i, bool mode) { userModes[i].second = mode; }
 
-void User::setRealname(std::string realname)
-{
-    this->realname = realname;
-}
+void User::setRealname(std::string realname) { this->realname = realname; }
 
-void User::clearBuffer()
-{
-    buff.clear();
-}
+void User::clearBuffer() { buff.clear(); }
 
 void User::addInvitation(std::string &channel)
 {
@@ -213,11 +148,11 @@ void User::addInvitation(std::string &channel)
 
 void User::removeInvitation(std::string &channel)
 {
-    for (size_t i = 0; i < this->invitation.size(); i++)
+    for (size_t i = 0; i < invitation.size(); ++i)
     {
-        if (this->invitation[i] == channel)
+        if (invitation[i] == channel)
         {
-            this->invitation.erase(this->invitation.begin() + i);
+            invitation.erase(invitation.begin() + i);
             return;
         }
     }
