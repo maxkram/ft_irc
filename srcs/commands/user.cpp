0b7a7ee94a7e8e1df645629c0de861efc15a3860
Maksim Kramer <mkramer@c1r1p5.42quebec.com>
2024-12-27 23:07:24 -0500

    update user

--- srcs/commands/user.cpp
+++ srcs/commands/user.cpp
@@ -2,11 +2,9 @@
 
 void	Server::USER(std::string &message, int fd)
 {
-	User	*user;
-	std::vector<std::string>	param;
+	User	*user = getClientByFd(fd);
+	std::vector<std::string>	param = extractParams(message);
 
-	user = getClientByFd(fd);
-	param = extractParams(message);
 	if (user && param.size() < 5)
 	{
 		notifyUsers(ERR_NOTENOUGHPARAMETERS(user->getNickname()), fd);
@@ -16,20 +14,21 @@ void	Server::USER(std::string &message, int fd)
 	{
 		notifyUsers(ERR_NOTREGISTERED(std::string("*")), fd);
 	}
-	else if (user && !user->getUser().empty())
+	if (!user->getUser().empty())
 	{
 		notifyUsers(ERR_ALREADYREGISTERED(user->getNickname()), fd);
 		return;
 	}
-	else
+	if (user)
 	{
 		user->setUser(param[1]);
-		std::string	realname = param[4];
-		realname = realname.substr(1);
+		std::string	realname = param[4].substr(1);
 		user->setRealname(realname);
 	}
 
-	if (user && user->isRegistered() && !user->getUser().empty() && !user->getNickname().empty() && user->getNickname() != "*" && !user->isConnected())
+	if (user && user->isRegistered() && !user->getUser().empty() &&
+		!user->getNickname().empty() && user->getNickname() != "*" &&
+		!user->isConnected())
 	{
 		user->setConnected(true);
 		notifyUsers(RPL_WELCOME(user->getNickname()), fd);
