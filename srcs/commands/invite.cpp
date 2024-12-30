491ab34c199dadefed08d2b2fb75d33105c391cf
Maksim Kramer <mkramer@c1r1p5.42quebec.com>
2024-12-29 22:21:55 -0500

    update invite

--- srcs/commands/invite.cpp
+++ srcs/commands/invite.cpp
@@ -2,53 +2,57 @@
 
 void Server::INVITE(std::string message, int fd)
 {
-    std::vector<std::string> param;
-    User *user;
-    Channel *channel;
-    std::string invitedUserName;
-    std::string channelName;
-
-    param = extractParams(message);
-    user = getClientByFd(fd);
+    std::vector<std::string> param = extractParams(message);
+    User *user = getClientByFd(fd);
 
     if (param.size() < 2)
     {
         notifyUsers(ERR_NOTENOUGHPARAMETERS(user->getNickname()), fd);
         return;
     }
+
+    std::string invitedUserName = param[1];
+    std::string channelName = param[2].substr(1);
+
+    // Validate channel existence
     if (!isChannelAvailable(param[2]))
     {
-        sendErrorToClient(403, getClientByFd(fd)->getNickname(), param[2], getClientByFd(fd)->getFduser(), " :No such channel\r\n");
+        sendErrorToClient(403, user->getNickname(), param[2], user->getFduser(), " :No such channel\r\n");
         return;
     }
 
-    invitedUserName = param[1];
-    channelName = param[2].substr(1);
+    Channel *channel = getChannel(channelName);
 
-    channel = getChannel(channelName);
+    // Check user membership in the channel
     if (!channel->isUserInChannel(user->getNickname()))
     {
-        sendErrorToClient(442, getClientByFd(fd)->getNickname(), channelName, getClientByFd(fd)->getFduser(), " :You're not on that channel\r\n");
+        sendErrorToClient(442, user->getNickname(), channelName, user->getFduser(), " :You're not on that channel\r\n");
         return;
     }
+
+    // Check if the user is a channel operator
     if (!channel->isUserOperator(user->getNickname()))
     {
-        sendErrorToClient(482, getClientByFd(fd)->getNickname(), channelName, getClientByFd(fd)->getFduser(), " :You're not channel operator\r\n");
+        sendErrorToClient(482, user->getNickname(), channelName, user->getFduser(), " :You're not channel operator\r\n");
         return;
     }
+
+    // Check if the invited user is already in the channel
     if (channel->isUserInChannel(invitedUserName))
     {
         notifyUsers(ERR_USERONCHANNEL(user->getNickname(), invitedUserName, channelName), fd);
         return;
     }
+
+    // Handle inviting the user
     User *invitedUser = getClientByNickname(invitedUserName);
     if (invitedUser != NULL)
     {
         invitedUser->addInvitation(channelName);
-        std::string notif;
-        notif = getClientByFd(fd)->getNickname() + " has invited you to the channel #" + channelName + "\r\n";
+
+        // Notify the invited user
+        std::string notif = user->getNickname() + " has invited you to the channel #" + channelName + "\r\n";
         notifyUsers(notif, invitedUser->getFduser());
-        
     }
     else
     {
